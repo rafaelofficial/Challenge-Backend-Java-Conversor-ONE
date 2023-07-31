f9b5877deec34c448dab243f19328c1820123b2b
Rafael Sousa Pereira <rafael.sousa.pereira@mail.usf.edu.br>
2023-07-31 05:20:14 -0300

    feat: implemented converter methods that convert BRL to another currencies

---
+++ conversor/src/br/com/one/conversor/moeda/services/ConversorMoedaService.java
@@ -0,0 +1,75 @@
+package br.com.one.conversor.moeda.services;
+
+import java.text.NumberFormat;
+import java.util.Locale;
+import java.util.Objects;
+
+import javax.swing.JOptionPane;
+
+import com.google.gson.JsonObject;
+
+import br.com.one.conversor.moeda.exception.ConversaoNaoExiste;
+
+public class ConversorMoedaService {
+
+	private JsonObject taxaCambio;
+
+	public ConversorMoedaService() {
+		CotacaoTempoRealService cotacaoService = new CotacaoTempoRealService();
+		this.taxaCambio = cotacaoService.buscarCotacao();
+	}
+
+	private String formatadorMoeda(Locale locale, double moeda) {
+		return NumberFormat.getCurrencyInstance(locale).format(moeda);
+	}
+
+	private double converterMoedas(String paresDeMoedas, double valor) {
+		if (Objects.nonNull(paresDeMoedas)) {
+			JOptionPane.showMessageDialog(null, "Conversão não existe, tente novamente", null,
+					JOptionPane.ERROR_MESSAGE);
+			throw new ConversaoNaoExiste("Conversão não existe");
+		}
+
+		double taxaCambioApi = this.taxaCambio.getAsJsonObject(paresDeMoedas.replace("-", "")).get("ask").getAsDouble();
+		return Math.round((valor * taxaCambioApi * 100d)) / 100;
+	}
+
+	/** converte Real para outras moedas */
+
+	public void conversorRealDolar(double valor) {
+		double realParaDolar = valor / converterMoedas("BRL-USD", valor);
+		String formatadorMoeda = formatadorMoeda(Locale.US, realParaDolar);
+
+		JOptionPane.showMessageDialog(null, "O valor da conversão é de " + formatadorMoeda + " Dólar Americano",
+				"Menssagem", JOptionPane.INFORMATION_MESSAGE);
+	}
+
+	public void conversorRealEuro(double valor) {
+		double realEuro = valor / converterMoedas("BRL-EUR", valor);
+		String formatadorMoeda = formatadorMoeda(Locale.FRANCE, realEuro);
+
+		JOptionPane.showMessageDialog(null, "O valor da conversão é de " + formatadorMoeda + " Euro", "Menssagem",
+				JOptionPane.INFORMATION_MESSAGE);
+	}
+
+	public void conversorRealLibra(double valor) {
+		double realLibra = valor / converterMoedas("BRL-GBP", valor);
+		String formatadorMoeda = formatadorMoeda(Locale.UK, realLibra);
+
+		JOptionPane.showMessageDialog(null, "O valor da conversão é de " + formatadorMoeda + " Libras Britânica",
+				"Menssagem", JOptionPane.INFORMATION_MESSAGE);
+
+	}
+
+	/** converte outras moedas para Real */
+
+	public void conversorDolarReal(double valor) {
+		double dolarReal = valor / converterMoedas("USD-BRL,", valor);
+		String formatadorMoeda = formatadorMoeda(Locale.ENGLISH, dolarReal);
+
+		JOptionPane.showMessageDialog(null, "O valor da conversão é de " + formatadorMoeda + " Real Brasileiro",
+				"Menssagem", JOptionPane.INFORMATION_MESSAGE);
+
+	}
+
+}
